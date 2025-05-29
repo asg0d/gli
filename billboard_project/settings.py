8127e842c2c3089987d98994b57753d949a18323
XAU/USD <64456107+asg0d@users.noreply.github.com>
2025-05-29 13:01:10 +0500

    congrugulation

--- billboard_project/settings.py
+++ billboard_project/settings.py
@@ -101,7 +101,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "location.utu-ranch.uz"
+    "https://location.utu-ranch.uz",
+    "https://gli-iota.vercel.app",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = DEBUG
--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:3333/api"
+const API_BASE_URL = "https://api.location.utu-ranch.uz/api"
 
 export interface Billboard {
   id: number
