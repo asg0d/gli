cf4137364ca53abb50b2688a323f9162dbcd0f02
XAU/USD <64456107+asg0d@users.noreply.github.com>
2025-05-29 22:31:12 +0500

    tetlu

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin", "cyrillic"] })
 
 export const metadata: Metadata = {
-  title: "Наши Локации | Эстетика и Функциональность",
-  description: "Уникальные пространства для вашего комфорта и вдохновения",
+  title: "Реклмные биллборды",
+  description: "Биллборды",
     generator: 'v0.dev'
 }
 
